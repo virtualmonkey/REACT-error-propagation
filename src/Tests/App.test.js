63b34f2c39db94d2b0da56f627e2fef56f870af7
suulcoder <saulcontreras@Sauls-MacBook-Air.local>
2022-11-01 15:07:48 -0600

    refactor: new structure

--- src/Tests/App.test.js
+++ src/Tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './containers/App';
+import App from '../containers/App';
 
 test('renders learn react link', () => {
   
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,14 +8,6 @@ const Header = () => {
                 {/* <img src="http://tachyons.io/img/logo.jpg" className="dib w2 h2 br-100" alt="Site Name"/> */}
                 <h3 className="light-gray dib mv2">Calculadora de Propagación de Error</h3>
             </div>
-            <div className="db dtc-l v-mid w-100 w-75-l tc tr-l">
-                {/* <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Home">Home</a>
-                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="How it Works">How it Works</a>
-                <a className="link dim dark-gray f6 f5-l dib mr3 mr4-l" href="#" title="Blog">Blog</a> */}
-                {/* <p className="dim light-gray f6 f5-l dib ma2">Made by:</p> */}
-                <a className="link dim light-gray f6 f5-l dib mr3 mr4-l" href="https://github.com/virtualmonkey" title="Developer" target="blank">Alejandro Urbina</a>
-                <a className="link dim light-gray f6 f5-l dib" href="https://github.com/suulcoder" title="Developer" target="blank">Saúl Contreras</a>
-            </div>
         </nav>
     );
 }
